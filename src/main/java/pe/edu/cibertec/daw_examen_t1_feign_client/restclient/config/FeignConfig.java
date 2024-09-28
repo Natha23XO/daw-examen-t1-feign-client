a026d025c5b76a83b391fc03de5147234a69fb11
Genesis <genesis@kaztro.cloud>
2024-09-27 20:18:48 -0500

    eliminado carpetas

--- src/main/java/pe/edu/cibertec/daw_examen_t1_feign_client/restclient/config/FeignConfig.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.cibertec.daw_examen_t1_feign_client.restclient.config;
-import feign.codec.ErrorDecoder;
-import org.springframework.context.annotation.Bean;
-import pe.edu.cibertec.daw_examen_t1_feign_client.restclient.errorhandler.CustomErrorDecoder;
-
-public class FeignConfig {
-
-}
--- src/main/java/pe/edu/cibertec/daw_examen_t1_feign_client/restclient/config/FeignInterceptorConfig.java
+++
@@ -1,4 +0,0 @@
-package pe.edu.cibertec.daw_examen_t1_feign_client.restclient.config;
-
-public class FeignInterceptorConfig {
-}
